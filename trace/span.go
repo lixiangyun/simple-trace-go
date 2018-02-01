2d4e7f7686bedf6b362fe3c8c1f7335ba3b5c60e
lixiangyun 00178505 <lixiangyun@huawei.com>
2018-02-01 16:35:28 +0800

    update

--- trace/span.go
+++ trace/span.go
@@ -1,5 +1,9 @@
 package trace
 
+import (
+	"log"
+)
+
 const (
 	CLIENT = "CLIENT"
 	SERVER = "SERVER"
@@ -95,4 +99,51 @@ func (s *Span) End() {
 	span.LocalEndpoint = s.edp
 
 	Collector(span)
+}
+
+const defaultBatchSize = 100 // the counts of span record
+
+var spanRecordChan chan *SpanRecord
+
+func init() {
+	spanRecordChan = make(chan *SpanRecord, defaultBatchSize)
+	go collectorLoop()
+}
+
+func collectorLoop() {
+
+	for {
+		span := <-spanRecordChan
+		count := len(spanRecordChan) + 1
+
+		buffer := make([]*SpanRecord, count)
+		buffer[0] = span
+
+		for i := 1; i < count; i++ {
+			buffer[i] = <-spanRecordChan
+		}
+
+		err := PostSpan(buffer)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
+}
+
+func Collector(s *SpanRecord) {
+	spanRecordChan <- s
+}
+
+type Context struct {
+	TraceID  string `json:"x-trace-id"`
+	SpanID   string `json:"x-span-id"`
+	ParentID string `json:"x-parent-id"`
+}
+
+func NewContext(pctx *Context) *Context {
+	if pctx != nil {
+		return &Context{TraceID: pctx.TraceID, ParentID: pctx.SpanID, SpanID: GetSpanID()}
+	} else {
+		return &Context{TraceID: GetTraceID(), SpanID: GetSpanID()}
+	}
 }
